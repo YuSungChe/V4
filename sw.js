61337ce926b43241e7d9277d7979d938675a2a28
rbklp7828 <rbklp7828@gmail.com>
2026-03-22 18:20:26 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'baccarat-v4';
+
+self.addEventListener('install', e => {
+    e.waitUntil(
+        caches.open(CACHE).then(cache => {
+            return cache.addAll([
+                './',
+                './index.html',
+                './manifest.json'
+            ]);
+        })
+    );
+});
+
+self.addEventListener('fetch', e => {
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    );
+});
